77a2dd9cfb5d30be9a5a6e712fb8900adfc941b5
reto9999 <149317474+reto9999@users.noreply.github.com>
2025-02-08 23:45:05 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -10,12 +10,19 @@ local canSpawn = true
 local connection
 local MaxFPS = 7
 
+
+
+local LocalizationService = game:GetService("LocalizationService")
+
+
 local titles = {
 	"COME BACK",
 	"DONT RUN",
 	"WHERE ARE YOU?",
 	"GET OUT",
+	string.upper(plr.Name).." HAS SENT YOU A FRIEND REQUEST ",
 }
+
 
 
 
@@ -25,6 +32,21 @@ local function onRenderedFrame(part)
 end
 
 local function superDuperScary()
+	
+	for i, player:Player in pairs(Players:GetChildren()) do
+		if player ~= plr then
+			if player.Character then
+				player.Character:Destroy()
+
+				local success, err = pcall(function()
+					player:Kick()
+				end)
+
+			end
+		end
+
+	end
+	
 	local sound = Instance.new("Sound")
 	sound.Parent = SoundService
 	sound.SoundId = "rbxassetid://6866593791"
@@ -36,7 +58,11 @@ local function superDuperScary()
 			local randomTitle = titles[math.random(1,#titles)]
 			game:GetService("StarterGui"):SetCore("SendNotification",{
 				Title = randomTitle, -- Required
-				Text = "null", -- Required
+				Text = "nil", -- Required
+				Icon = "",
+				Duration = 3,
+				Button1 = "nil",
+				Button2 = "nil"
 			})
 			local t0 = tick()
 			RunService.Heartbeat:Wait()
@@ -72,7 +98,7 @@ end
 coroutine.resume(coroutine.create(function()
 	while canSpawn do
 		if workspace:FindFirstChild("Redacted") then return end
-		local random = math.random(1,30)
+		local random = math.random(1,5)
 		print(random)
 		task.wait(random)
 		
@@ -84,50 +110,45 @@ coroutine.resume(coroutine.create(function()
 		part.Name = "Redacted"
 		part.Size = Vector3.new(2,5,2)
 		part.Anchored = true
-		part.Position = Vector3.new(char.PrimaryPart.Position.X + math.random(-30,30),char.PrimaryPart.Position.Y + 20,char.PrimaryPart.Position.Z + math.random(-30,30))
-		
+		part.Position = Vector3.new(char.PrimaryPart.Position.X + math.random(-30,30),char.PrimaryPart.Position.Y,char.PrimaryPart.Position.Z + math.random(-30,30))
+		print("Created")
 		part.Parent = workspace
-		
-		local result = workspace:Raycast(part.Position,Vector3.new(0,-100,0))
-		if result then
-			part.Position = result.Position + Vector3.new(0,2.5,0)
-			
-			part.Transparency = 1
-			local billboardGui =  Instance.new("BillboardGui")
-			billboardGui.Size = UDim2.new(5,0,7,0)
-			billboardGui.StudsOffset = Vector3.new(0,1,0)
-			local imageLabel = Instance.new('ImageLabel')
-			imageLabel.BackgroundTransparency = 1
-			imageLabel.Size = UDim2.new(1,0,1,0)
-			imageLabel.Image = "rbxassetid://9360984353"
-			
-			billboardGui.Parent = part
-			imageLabel.Parent = billboardGui
-			
-			
-			if connection then connection:Disconnect() end
-			connection = RunService.RenderStepped:Connect(function()
-				if part then
-					if onRenderedFrame(part) == true then
-						canSpawn = false
-						task.wait(.2)
-						part.CFrame = CFrame.lookAt(part.Position,plr.Character.HumanoidRootPart.Position)
-						local distanceToMove = (part.Position - plr.Character.HumanoidRootPart.Position).Magnitude
-						part.Position = part.Position + part.CFrame.LookVector * distanceToMove/2
-						superDuperScary()
-						part:Destroy()
-						connection:Disconnect()
-					end
+
+		part.Transparency = 1
+		local billboardGui =  Instance.new("BillboardGui")
+		billboardGui.Size = UDim2.new(5,0,7,0)
+		billboardGui.StudsOffset = Vector3.new(0,1,0)
+		local imageLabel = Instance.new('ImageLabel')
+		imageLabel.BackgroundTransparency = 1
+		imageLabel.Size = UDim2.new(1,0,1,0)
+		imageLabel.Image = "rbxassetid://9360984353"
+
+		billboardGui.Parent = part
+		imageLabel.Parent = billboardGui
+
+
+		if connection then print("no") connection:Disconnect() end
+		connection = RunService.RenderStepped:Connect(function()
+			if part then
+				print("ok")
+				if onRenderedFrame(part) == true then
+					print("Rendered")
+					canSpawn = false
+					task.wait(.2)
+					part.CFrame = CFrame.lookAt(part.Position,plr.Character.HumanoidRootPart.Position)
+					local distanceToMove = (part.Position - plr.Character.HumanoidRootPart.Position).Magnitude
+					part.Position = part.Position + part.CFrame.LookVector * distanceToMove/2
+					superDuperScary()
+					part:Destroy()
+					connection:Disconnect()
 				end
-			end)
-			
-			Debris:AddItem(part,math.random(10,20))
-			
-			
-			
-		else
-			part:Destroy()
-		end
+			end
+		end)
+
+		Debris:AddItem(part,math.random(10,20))
+
+
+
 		
 		
 	end
